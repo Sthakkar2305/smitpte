fc0f626c84c2bc11c3ee4eacf0dd4168b90a6fa3
smit thakkar <sthakkar837@gmail.com>
2025-08-21 00:42:21 +0530

    final update

--- models/Task.js
+++ models/Task.js
@@ -1,65 +1,71 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const taskSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true
+    required: true,
   },
   type: {
     type: String,
     required: true,
     enum: [
-      'Personal Introduction',
-      'Read Aloud',
-      'Repeat Sentence',
-      'Describe Image',
-      'Retell Lecture',
-      'Answer Short Question',
-      'Summarize Written Text',
-      'Essay',
-      'Multiple Choice, Choose Single Answer',
-      'Multiple Choice, Choose Multiple Answers',
-      'Re-order Paragraphs',
-      'Reading Fill in the Blanks',
-      'Reading & Writing Fill in the Blanks',
-      'Summarize Spoken Text',
-      'Multiple Choice, Choose Multiple Answers (Listening)',
-      'Fill in the Blanks (Listening)',
-      'Highlight Correct Summary',
-      'Multiple Choice, Choose Single Answer (Listening)',
-      'Select Missing Word',
-      'Highlight Incorrect Words',
-      'Write from Dictation'
-    ]
+      "Personal Introduction",
+      "Read Aloud",
+      "Repeat Sentence",
+      "Describe Image",
+      "Retell Lecture",
+      "summarize groud desicusion",
+      "response to situation",
+      "Answer Short Question",
+      "Summarize Written Text",
+      "Essay",
+      "Multiple Choice, Choose Single Answer",
+      "Multiple Choice, Choose Multiple Answers",
+      "Re-order Paragraphs",
+      "Reading Fill in the Blanks",
+      "Reading & Writing Fill in the Blanks",
+      "Summarize Spoken Text",
+      "Multiple Choice, Choose Multiple Answers (Listening)",
+      "Fill in the Blanks (Listening)",
+      "Highlight Correct Summary",
+      "Multiple Choice, Choose Single Answer (Listening)",
+      "Select Missing Word",
+      "Highlight Incorrect Words",
+      "Write from Dictation",
+      "section test",
+      "full mock test",
+    ],
   },
   description: {
     type: String,
-    required: true
+    required: true,
   },
   quantity: {
     type: Number,
-    default: 1
+    default: 1,
   },
   deadline: {
-    type: Date
+    type: Date,
   },
-  assignedTo: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }],
+  assignedTo: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
   createdBy: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+    ref: "User",
+    required: true,
   },
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   isActive: {
     type: Boolean,
-    default: true
-  }
+    default: true,
+  },
 });
 
-export default mongoose.models.Task || mongoose.model('Task', taskSchema);
+export default mongoose.models.Task || mongoose.model("Task", taskSchema);
